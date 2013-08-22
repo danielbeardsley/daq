35f5f2cf8572d4134de3fe3503e826729cb4826c
Daniel Beardsley <daniel@ifixit.com>
2013-08-22 03:09:19 -0700

    Get the basics going and passing tests
    jobs in, jobs out.

--- queue.js
+++ queue.js
@@ -1,9 +1,11 @@
 var net = require('net');
 var Q = require('q');
+var NotifyQueue = require('notify-queue');
 
-function DumbAssQueue() {
+module.exports = function DumbAssQueue() {
   var producerServer = net.createServer();
   var consumerServer = net.createServer();
+  var queue = new NotifyQueue();
 
   this.listen = function listen(portProducer, portConsumer) {
     var promise = Q.all([
@@ -12,6 +14,9 @@ function DumbAssQueue() {
     ]);
     producerServer.listen(portProducer);
     consumerServer.listen(portConsumer);
+
+    setupConnectionHandlers();
+
     return promise;
   }
 
@@ -19,6 +24,42 @@ function DumbAssQueue() {
     producerServer.close();
     consumerServer.close();
   }
+
+  function setupConnectionHandlers () {
+    producerServer.on('connection', function(socket) {
+      log("producer connected");
+      forEachJsonObject(socket, function(object) {
+        log("job received from producer: " + JSON.stringify(object));
+        queue.push(object);
+      });
+    });
+
+    consumerServer.on('connection', function(socket) {
+      log("consumer connected");
+      var cancel = queue.pop(function(object, done) {
+        log("sending job to consumer");
+        socket.write(JSON.stringify(object) + "\n");
+        done();
+      });
+
+      socket.on('close', cancel);
+    });
+
+  }
 }
   
-module.exports = DumbAssQueue;
+function forEachJsonObject(socket, callback) {
+  var data = '';
+  socket.on('data', function(chunk) {
+    log("chunk:" + chunk + " data:" + data);
+    data += chunk.toString();
+    if (data[data.length-1] == "\n") {
+      callback(JSON.parse(data.toString()));
+      data = '';
+    }
+  });
+}
+
+function log(msg) {
+  // console.log(msg);
+}
--- test.js
+++ test.js
@@ -3,22 +3,35 @@ var assert  = require('assert');
 var Q       = require('q');
 var Queue   = require('./queue.js');
 
-var port    = 34595;
-var port2   = 34596;
+var portP   = 34595;
+var portC   = 34596;
 
 describe('Queue', function(){
-  var q = new Queue();
   describe("network interface", function() {
     it('should allow connections', function(done) {
-      q.listen(port, port2).then(function() {
+      var q = new Queue();
+      q.listen(portP, portC).then(function() {
         return Q.all([
-          testConnectionToPort(port),
-          testConnectionToPort(port2)
+          testConnectionToPort(portP),
+          testConnectionToPort(portC)
         ]);
       }).then(function teardown() {
         q.close();
         done();
-      });
+      }).done();
+    });
+
+    it('should pass jobs', function(done) {
+      var q = new Queue();
+      q.listen(portP, portC).then(function() {
+        log("Listening on both ports");
+        return sendAJob({type:"blah"});
+      }).
+      then(receiveAJob).
+      then(function () {
+        q.close();
+        done();
+      }).done();
     });
   });
 })
@@ -27,7 +40,40 @@ function testConnectionToPort(port) {
   var deferred = Q.defer();
   var connection = net.connect(port, null, function() {
     connection.end();
-    deferred.resolve(true);
+    deferred.resolve();
   });
   return deferred.promise;
+}
+
+function sendAJob(job) {
+  var deferred = Q.defer();
+  log("connecting producer");
+  var connection = net.connect(portP, null, function() {
+    log('sending as producer');
+    connection.end(JSON.stringify(job) + "\n");
+    log('resolving');
+    deferred.resolve();
+  });
+  return deferred.promise;
+}
+
+function receiveAJob(job) {
+  var deferred = Q.defer();
+  var result = '';
+  var connection = net.connect(portC);
+
+  log("connecting consumer");
+  connection.on('data', function(data) {
+    log('received on consumer');
+    result += data.toString();
+    if (result[result.length-1] == "\n") {
+      deferred.resolve(JSON.parse(result.toString()));
+      connection.end();
+    }
+  });
+  return deferred.promise;
+}
+
+function log(msg) {
+  // console.log(msg);
 }
